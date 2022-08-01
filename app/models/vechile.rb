9e5d1e304cb18ee810605398bb5cfdd2e40b6784
raviw-2022 <ravi.s@webkorps.com>
2022-08-01 23:35:51 +0530

    Done

--- app/models/vechile.rb
+++ app/models/vechile.rb
@@ -1,5 +1,6 @@
 class Vechile < ApplicationRecord
   belongs_to :employee
   scope :search, lambda {|query|where(["name LIKE?","%#{query}%"])}   
+  validates :number, presence: true, uniqueness: true
   # Ex:- scope :active, -> {where(:active => true)}
 end
